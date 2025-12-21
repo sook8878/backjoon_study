73ebed99c1a2f7504835998f579bfc4ea5115f8e
sook8878 <qpwlxk123@gmail.com>
2025-12-22 02:10:18 +0900

    2588. 곱셈

--- src/class1/Main.java
+++ src/class1/Main.java
@@ -6,17 +6,20 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+//        String str = br.readLine();
 
-        StringTokenizer st = new StringTokenizer(str);
+//        StringTokenizer st = new StringTokenizer(str);
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
 
-        System.out.println((a + b) % c);
-        System.out.println(((a % c) + (b % c)) % c);
-        System.out.println((a * b) % c);
-        System.out.println(((a % c) * (b % c)) % c);
+        int unit = num2 % 10;
+        int ten = (num2 % 100) / 10;
+        int hundred = num2 / 100;
+
+        System.out.println(num1 * unit);
+        System.out.println(num1 * ten);
+        System.out.println(num1 * hundred);
+        System.out.println(num1 * num2);
     }
 }
