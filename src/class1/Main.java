e215af928cfbd4bcd3cc38eed3554d2d72378d19
sook8878 <qpwlxk123@gmail.com>
2025-12-22 17:02:55 +0900

    10171. 고양이

--- src/class1/Main.java
+++ src/class1/Main.java
@@ -5,14 +5,9 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str);
-
-        long num1 = Long.parseLong(st.nextToken());
-        long num2 = Long.parseLong(st.nextToken());
-        long num3 = Long.parseLong(st.nextToken());
-
-        System.out.println(num1 + num2 + num3);
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
     }
 }
