501f59bdf6722d4b00ea73740e157a98336a8563
sook8878 <qpwlxk123@gmail.com>
2025-12-24 12:50:50 +0900

    14681. 사분면 고르기

--- src/class2/Main.java
+++ src/class2/Main.java
@@ -6,18 +6,23 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str);
+//        String str = br.readLine();
+//        StringTokenizer st = new StringTokenizer(str);
 
-        int num1 = Integer.parseInt(st.nextToken());
-
-        boolean isLeapYear = (num1 % 4 == 0 && num1 % 100 != 0) || (num1 % 4 == 0 && num1 % 400 == 0);
+//        int x = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(br.readLine());
+//        int y = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(br.readLine());
 
         String result;
-        if(isLeapYear){
+        if(x > 0 && y > 0){
             result = "1";
+        } else if (x < 0 && y > 0){
+            result = "2";
+        } else if (x < 0 && y < 0) {
+            result = "3";
         } else {
-            result = "0";
+            result = "4";
         }
 
         System.out.println(result);
