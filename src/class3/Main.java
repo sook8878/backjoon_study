7f9306a2bc0c3db9912495524061c43418dd596b
sook8878 <qpwlxk123@gmail.com>
2025-12-27 21:18:40 +0900

    8393. 합

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -3,21 +3,17 @@ package class3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for(int i = 0; i < t; i++){
-            String line = br.readLine();
-            StringTokenizer st = new StringTokenizer(line);
+        int n = Integer.parseInt(br.readLine());
 
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            System.out.println(a+b);
+        int sum = 0;
+        for(int i = 0; i <= n ; i++){
+            sum += i;
         }
+        System.out.println(sum);
     }
 }
