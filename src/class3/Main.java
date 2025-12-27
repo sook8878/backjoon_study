d6d3f1bc3c496a2af416f967e6cac2a83d02225c
sook8878 <qpwlxk123@gmail.com>
2025-12-27 21:12:48 +0900

    10950. A+B - 3

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -3,17 +3,21 @@ package class3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str);
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 0; i < t; i++){
+            String line = br.readLine();
+            StringTokenizer st = new StringTokenizer(line);
 
-        int n = Integer.parseInt(st.nextToken());
-        for(int i = 1; i < 10; i ++){
-            System.out.println(n + " * " + i + " = " + (n * i));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(a+b);
         }
     }
 }
