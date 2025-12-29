75f93ff6b7623361d479ec9dddbbe3f3e625a66a
sook8878 <qpwlxk123@gmail.com>
2025-12-29 17:18:10 +0900

    15552. 빠른 A+B

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,21 +1,23 @@
 package class3;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int loopCount = n/4;
-        String result = "";
-        for(int i = 0; i < loopCount; i++){
-            result += "long ";
+        int t = Integer.parseInt(br.readLine());
+        int a=0;
+        int b=0;
+        for(int i = 0; i<t; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            bw.write(a + b + "\n");
         }
 
-        System.out.println(result + "int");
+        bw.close();
     }
 }
