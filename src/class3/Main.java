651eac627825a9a5228a53034ba6142c6bf1007b
sook8878 <qpwlxk123@gmail.com>
2026-01-02 17:17:34 +0900

    10871. X보다 작은 수

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,27 +1,47 @@
 package class3;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 1. 내 제출
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        StringTokenizer firstLine = new StringTokenizer(br.readLine());
+//        int n = Integer.parseInt(firstLine.nextToken());
+//        int x = Integer.parseInt(firstLine.nextToken());
+//
+//        StringTokenizer secondLine = new StringTokenizer(br.readLine());
+//        List<String> numbers = new ArrayList<>();
+//        for(int i = 0; i < n; i ++){
+//            int number = Integer.parseInt(secondLine.nextToken());
+//            if(number < x){
+//                numbers.add(String.valueOf(number));
+//            }
+//        }
+//
+//        bw.write(String.join(" ", numbers));
+//        bw.close();
+
+        // 2. 개선 (참고)
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
+        String[] arr = br.readLine().split(" ");
+        int n = Integer.parseInt(arr[0]);
+        int x = Integer.parseInt(arr[1]);
 
-        String numbers = br.readLine();
-        StringTokenizer st = new StringTokenizer(numbers);
-
-        int v = Integer.parseInt(br.readLine());
-        int count = 0;
-        for(int i = 0; i < t; i ++){
-            int number = Integer.parseInt(st.nextToken());
-            if(number == v){
-                count += 1;
+        String[] arr2 = br.readLine().split(" ");
+        for(int i = 0; i < n; i ++){
+            if(Integer.parseInt(arr2[i]) < x){
+                bw.write(arr2[i] + " ");
             }
         }
-        bw.write(count + "");
+
         bw.close();
     }
 }
