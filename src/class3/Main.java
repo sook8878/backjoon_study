9f1fa002df15f9259a8dcfddc92a62ee0d5cffca
sook8878 <qpwlxk123@gmail.com>
2026-01-02 17:25:57 +0900

    10818. 최소, 최대

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,47 +1,30 @@
 package class3;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 1. 내 제출
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//
-//        StringTokenizer firstLine = new StringTokenizer(br.readLine());
-//        int n = Integer.parseInt(firstLine.nextToken());
-//        int x = Integer.parseInt(firstLine.nextToken());
-//
-//        StringTokenizer secondLine = new StringTokenizer(br.readLine());
-//        List<String> numbers = new ArrayList<>();
-//        for(int i = 0; i < n; i ++){
-//            int number = Integer.parseInt(secondLine.nextToken());
-//            if(number < x){
-//                numbers.add(String.valueOf(number));
-//            }
-//        }
-//
-//        bw.write(String.join(" ", numbers));
-//        bw.close();
-
-        // 2. 개선 (참고)
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] arr = br.readLine().split(" ");
-        int n = Integer.parseInt(arr[0]);
-        int x = Integer.parseInt(arr[1]);
+        int n = Integer.parseInt(br.readLine());
 
-        String[] arr2 = br.readLine().split(" ");
+        String[] numbers = br.readLine().split(" ");
+        int min = 0;
+        int max = 0;
         for(int i = 0; i < n; i ++){
-            if(Integer.parseInt(arr2[i]) < x){
-                bw.write(arr2[i] + " ");
+            int number = Integer.parseInt(numbers[i]);
+
+            if(min >= number || min == 0){
+                min = number;
+            }
+
+            if(max <= number || max == 0){
+                max = number;
             }
         }
 
+        bw.write(min + " " + max);
         bw.close();
     }
 }
