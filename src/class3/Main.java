d0b0c1b392ed946cdd1ea8ed5272d9a917fdbb3c
sook8878 <qpwlxk123@gmail.com>
2026-01-03 18:39:21 +0900

    2562. 최댓값

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,35 +1,24 @@
 package class3;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer firstLine = new StringTokenizer(br.readLine());
-        int buckets = Integer.parseInt(firstLine.nextToken());
-        int throwCount = Integer.parseInt(firstLine.nextToken());
+        int index = 0;
+        int maxNum = 0;
+        for(int i = 0; i < 9; i ++){
+            int n = Integer.parseInt(br.readLine());
 
-        int[] buketStatus = new int[buckets];
-        for(int i = 0; i < throwCount; i++){
-            StringTokenizer line = new StringTokenizer(br.readLine());
-            int startBuketNum = Integer.parseInt(line.nextToken());
-            int secBucketNum = Integer.parseInt(line.nextToken());
-            int ballNum = Integer.parseInt(line.nextToken());
-
-            int loopCnt = secBucketNum - startBuketNum + 1; //3
-            for(int j = 0; j < loopCnt; j++){
-                int changeIndex = (startBuketNum + j) - 1;
-                buketStatus[changeIndex] = ballNum;
+            if(maxNum <= n || maxNum == 0){
+                maxNum = n;
+                index = i+1;
             }
         }
 
-        for(int i = 0; i < buketStatus.length; i ++){
-            System.out.print(buketStatus[i] + " ");
-        }
-
+        bw.write(maxNum + "\n" + index);
         bw.close();
     }
 }
