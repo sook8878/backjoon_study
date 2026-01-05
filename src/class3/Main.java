42098c83d5ef9bdbb1cb35257a44d37644059896
sook8878 <qpwlxk123@gmail.com>
2026-01-05 22:21:35 +0900

    3052. 나머지

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,26 +1,23 @@
 package class3;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        List<String> studentsList = new ArrayList<>();
-        for(int i = 0; i < 30; i++){
-            studentsList.add((i + 1) + "");
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < 10; i++){
+            int n = Integer.parseInt(br.readLine());
+            int mode = n % 42;
+            map.put(mode, mode);
         }
 
-        for(int i = 0; i < 28; i ++){
-            studentsList.remove(br.readLine());
-        }
+        bw.write(map.size() + "");
 
-        for(String n : studentsList){
-            bw.write(n + "\n");
-        }
         bw.close();
     }
 }
