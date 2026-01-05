f3f33a67bee91d4a7d0402ec1d721949df65b3ea
sook8878 <qpwlxk123@gmail.com>
2026-01-05 22:00:24 +0900

    10813. 공 바꾸기

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,24 +1,38 @@
 package class3;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int index = 0;
-        int maxNum = 0;
-        for(int i = 0; i < 9; i ++){
-            int n = Integer.parseInt(br.readLine());
+        StringTokenizer firstLine = new StringTokenizer(br.readLine());
+        int buckets = Integer.parseInt(firstLine.nextToken());
+        int changeCount = Integer.parseInt(firstLine.nextToken());
 
-            if(maxNum <= n || maxNum == 0){
-                maxNum = n;
-                index = i+1;
-            }
+        int[] bucketStatus = new int[buckets];
+        for(int i = 0; i<bucketStatus.length; i++){
+            bucketStatus[i] = i+1;
         }
 
-        bw.write(maxNum + "\n" + index);
+        for(int i = 0; i < changeCount; i++){
+            StringTokenizer line = new StringTokenizer(br.readLine());
+            int startBuketNum = Integer.parseInt(line.nextToken());
+            int secBucketNum = Integer.parseInt(line.nextToken());
+
+            int startBuketNumOrigin = bucketStatus[startBuketNum-1];
+            int secBucketNumOrigin = bucketStatus[secBucketNum-1];
+
+            bucketStatus[startBuketNum-1] = secBucketNumOrigin;
+            bucketStatus[secBucketNum-1] = startBuketNumOrigin;
+        }
+
+        for(int i = 0; i < bucketStatus.length; i ++){
+            bw.write(bucketStatus[i] + " ");
+        }
+
         bw.close();
     }
 }
