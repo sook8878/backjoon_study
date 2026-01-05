32b5f19934db3bffa3d9fef5ed0200644a52d20e
sook8878 <qpwlxk123@gmail.com>
2026-01-05 23:41:30 +0900

    10811. 바구니 뒤집기

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,22 +1,42 @@
 package class3;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < 10; i++){
-            int n = Integer.parseInt(br.readLine());
-            int mode = n % 42;
-            map.put(mode, mode);
+        StringTokenizer firstLine = new StringTokenizer(br.readLine());
+        int buckets = Integer.parseInt(firstLine.nextToken());
+        int changeCount = Integer.parseInt(firstLine.nextToken());
+
+        int[] bucketStatus = new int[buckets];
+        for(int i = 0; i<bucketStatus.length; i++){
+            bucketStatus[i] = i+1;
         }
 
-        bw.write(map.size() + "");
+        for(int i = 0; i < changeCount; i++){
+            StringTokenizer line = new StringTokenizer(br.readLine());
+            int startBuketNum = Integer.parseInt(line.nextToken());
+            int secBucketNum = Integer.parseInt(line.nextToken());
+
+            for(int j = 0; j < (secBucketNum - startBuketNum); j++){
+                int startIndex = startBuketNum - 1 + j;
+                int secIndex = secBucketNum - 1 - j;
+                if(startIndex > secIndex){
+                    continue;
+                }
+                int startBuketNumOrigin = bucketStatus[startIndex];
+                int secBucketNumOrigin = bucketStatus[secIndex];
+                bucketStatus[startIndex] = secBucketNumOrigin;
+                bucketStatus[secIndex] = startBuketNumOrigin;
+            }
+        }
+        for(int i = 0; i < bucketStatus.length; i ++){
+            bw.write(bucketStatus[i] + " ");
+        }
 
         bw.close();
     }
