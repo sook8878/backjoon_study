ff8fcc412b917efaca9c35858199ce7699c338c9
sook8878 <qpwlxk123@gmail.com>
2026-01-06 19:44:34 +0900

    27866. 문자와 문자열

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,38 +1,16 @@
 package class3;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int i = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] scoreArray = new int[n];
-        int highestScore = 0;
-        for(int i = 0; i < n; i ++){
-            int score = Integer.parseInt(st.nextToken());
-            scoreArray[i] = score;
-
-            if(highestScore <= score || highestScore == 0){
-                highestScore = score;
-            }
-        }
-
-        double[] fakeScoreArray = new double[n];
-        for(int i = 0; i < n; i ++){
-            fakeScoreArray[i] = ((double) scoreArray[i] / highestScore * 100);
-        }
-
-        double average = 0;
-        for(double i : fakeScoreArray){
-            average += i;
-        }
-
-        bw.write(String.valueOf(average / n));
+        bw.write(s.substring(i - 1, i));
         bw.close();
     }
 }
