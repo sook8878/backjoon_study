b7c970647870aa154a0df15b804d6fcdc685a03e
sook8878 <qpwlxk123@gmail.com>
2026-01-09 21:10:04 +0900

    11654. 아스키 코드

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -5,11 +5,9 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
 
-        int t = Integer.parseInt(br.readLine());
-        for(int i = 0; i < t; i++){
-            String s = br.readLine();
-            System.out.println(s.substring(0,1) + "" + s.substring(s.length()-1, s.length()));
-        }
+        int ascii = str.charAt(0);
+        System.out.println(ascii);
     }
 }
