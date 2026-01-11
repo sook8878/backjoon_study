9f9eb4908e69c17a2efc74377b52f9d3b1b52bf6
sook8878 <qpwlxk123@gmail.com>
2026-01-11 21:22:42 +0900

    2908. 상수

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,19 +1,37 @@
 package class3;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String sentence = br.readLine().trim();
-        String[] wordArray = sentence.split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int result = wordArray.length;
-        if(sentence.isBlank()){
-            result = 0;
+
+        int num1 = reverseNum(st.nextToken());
+        int num2 = reverseNum(st.nextToken());
+
+        if(num1 > num2) {
+            System.out.println(num1);
+        } else {
+            System.out.println(num2);
+        }
+    }
+
+    public static int reverseNum(String originNum){
+        String[] num1StrArray = originNum.split("");
+        String str1 = num1StrArray[0];
+        String str3 = num1StrArray[2];
+        num1StrArray[0] = str3;
+        num1StrArray[2] = str1;
+
+        String numStr = "";
+        for(String num : num1StrArray){
+            numStr += num;
         }
 
-        System.out.println(result);
+        return Integer.parseInt(numStr);
     }
 }
