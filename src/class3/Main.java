871d3cc101973b138b2729b683cf2b22c769d1c7
sook8878 <qpwlxk123@gmail.com>
2026-01-12 01:07:42 +0900

    3003. 킹, 퀸, 룩, 비숍, 나이트, 폰

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,14 +1,30 @@
 package class3;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String readLine;
-        while ((readLine = br.readLine()) != null){
-            System.out.println(readLine);
+        int k = pieceTrans(Integer.parseInt(st.nextToken()), 1);
+        int q = pieceTrans(Integer.parseInt(st.nextToken()), 1);
+        int r = pieceTrans(Integer.parseInt(st.nextToken()), 2);
+        int b = pieceTrans(Integer.parseInt(st.nextToken()), 2);
+        int kn = pieceTrans(Integer.parseInt(st.nextToken()), 2);
+        int p = pieceTrans(Integer.parseInt(st.nextToken()), 8);
+
+        System.out.println(k + " " + q + " " + r + " " + b + " " + kn + " " + p + " " );
+    }
+
+    public static int pieceTrans(int currentPieceCount, int needPieceCount){
+        if(currentPieceCount > needPieceCount){
+            return -(currentPieceCount - needPieceCount);
+        } else if (currentPieceCount < needPieceCount) {
+            return needPieceCount - currentPieceCount;
+        } else {
+            return 0;
         }
     }
 }
