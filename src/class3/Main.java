f4d7a4555bc3e368bc43e5627a5aaf56e364a8a5
sook8878 <qpwlxk123@gmail.com>
2026-01-12 00:50:34 +0900

    11718. 그대로 출력하기

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -6,22 +6,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String word = br.readLine();
-        String[] wordArray = word.split("");
-
-        int time = 0;
-        for(int i = 0; i < wordArray.length; i++){
-            int ascii = word.charAt(i);
-            if(ascii == 83 || ascii >= 86){
-                ascii -= 1;
-                if(ascii >= 89){
-                    ascii -= 1;
-                }
-            }
-
-            int dialNumber = ((ascii - 65) / 3) + 2;
-            time += (dialNumber + 1);
+        String readLine;
+        while ((readLine = br.readLine()) != null){
+            System.out.println(readLine);
         }
-        System.out.println(time);
     }
 }
