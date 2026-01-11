54410cdd03e2863b26d375fe940405f78b64483e
sook8878 <qpwlxk123@gmail.com>
2026-01-11 21:02:44 +0900

    1152. 단어의 개수

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,27 +1,19 @@
 package class3;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < t; i ++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken());
-            String str = st.nextToken();
+        String sentence = br.readLine().trim();
+        String[] wordArray = sentence.split(" ");
 
-            String result = "";
-            for(int k = 0; k < str.length(); k ++){
-                String ch = str.substring(k, k+1);
-                for (int j = 0; j < r; j ++){
-                    result = result + ch;
-                }
-            }
-            System.out.println(result);
+        int result = wordArray.length;
+        if(sentence.isBlank()){
+            result = 0;
         }
 
+        System.out.println(result);
     }
 }
