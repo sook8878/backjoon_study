9c3d4490aeb8245c073c2e045f6ead9f3f42e6e8
sook8878 <qpwlxk123@gmail.com>
2026-01-13 19:15:55 +0900

    2444. 별 찍기 - 7

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,30 +1,35 @@
 package class3;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int k = pieceTrans(Integer.parseInt(st.nextToken()), 1);
-        int q = pieceTrans(Integer.parseInt(st.nextToken()), 1);
-        int r = pieceTrans(Integer.parseInt(st.nextToken()), 2);
-        int b = pieceTrans(Integer.parseInt(st.nextToken()), 2);
-        int kn = pieceTrans(Integer.parseInt(st.nextToken()), 2);
-        int p = pieceTrans(Integer.parseInt(st.nextToken()), 8);
+        int n = Integer.parseInt(br.readLine());
+        int loopCnt = n * 2;
 
-        System.out.println(k + " " + q + " " + r + " " + b + " " + kn + " " + p + " " );
-    }
+        for(int i = 1; i < n * 2; i++){
+            String starLine = "";
 
-    public static int pieceTrans(int currentPieceCount, int needPieceCount){
-        if(currentPieceCount > needPieceCount){
-            return -(currentPieceCount - needPieceCount);
-        } else if (currentPieceCount < needPieceCount) {
-            return needPieceCount - currentPieceCount;
-        } else {
-            return 0;
+            int checkCnt = loopCnt - i;
+            int starCnt;
+            if(checkCnt > n){
+                starCnt =  i * 2 - 1;
+            } else if (checkCnt < n) {
+                starCnt = (loopCnt - i) * 2 - 1;
+            } else {
+                starCnt = loopCnt - 1;
+            }
+
+            int blankCnt = (loopCnt - 1 - starCnt) / 2;
+            for(int j = 0; j < blankCnt; j++){
+                starLine += " ";
+            }
+            for(int j = 0; j < starCnt; j++){
+                starLine += "*";
+            }
+            System.out.println(starLine);
         }
     }
 }
