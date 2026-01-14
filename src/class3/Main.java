e89ea3a404dd767fc8f83b9f79c977b3d2317524
sook8878 <qpwlxk123@gmail.com>
2026-01-15 01:36:31 +0900

    1157. 단어 공부

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,19 +1,38 @@
 package class3;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String originWord = br.readLine();
-        String[] originWordArray = originWord.split("");
+        String originWord = br.readLine().toUpperCase();
+        String[] wordSplit = originWord.split("");
 
-        String reverseWord = "";
-        for(int i = originWordArray.length - 1; i >= 0; i --){
-            String word = originWordArray[i];
-            reverseWord += word;
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        for (String splitWord : wordSplit) {
+            Integer wordCount = wordCountMap.get(splitWord);
+            if(wordCount == null){
+                wordCountMap.put(splitWord, 1);
+            } else {
+                wordCountMap.put(splitWord, ++wordCount);
+            }
+
         }
-        System.out.println(originWord.equals(reverseWord) ? "1" : "0");
+
+        int maxCount = 0;
+        String maxWord = "";
+        for(String key : wordCountMap.keySet()){
+            int cnt = wordCountMap.get(key);
+            if(cnt > maxCount){
+                maxCount = cnt;
+                maxWord = key;
+            } else if (cnt == maxCount) {
+                maxWord += key;
+            }
+        }
+        System.out.println(maxWord.length() > 1 ? "?" : maxWord);
     }
 }
