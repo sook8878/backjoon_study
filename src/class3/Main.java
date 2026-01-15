ce1e5390ba59fe8d19bac22ef3cd507e2a6525c4
sook8878 <qpwlxk123@gmail.com>
2026-01-15 18:00:20 +0900

    1316. 그룹 단어 체커

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -5,17 +5,45 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String croatiaWord = br.readLine();
+        int t = Integer.parseInt(br.readLine());
 
-        String[] croatiaAlphabetArray = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+        int notDuplicateWordCnt = 0;
+        for(int i = 0; i < t; i++){
+            String word = br.readLine();
 
-        int croWordLength = 0;
-        for(String croAlphabet : croatiaAlphabetArray){
-            int originCroWordLength = croatiaWord.replaceAll(" ", "").length();
-            croatiaWord = croatiaWord.replaceAll(croAlphabet," ");
+            String[] wordArray = word.split("");
 
-            croWordLength = croWordLength + ((originCroWordLength - croatiaWord.replaceAll(" ", "").length()) / croAlphabet.length());
+            boolean check = false;
+            for(int j = 0; j < wordArray.length; j++){
+                check = false;
+                String currentWord = wordArray[j];
+
+                // 1. currentWord의 갯수 알아내기
+                String replaceWord = word.replaceAll(currentWord, "");
+                int currentWordCnt = word.length() - replaceWord.length();
+
+                // 2. 마지막인덱스 - 퍼스트 인덱스 != currentWord의 갯수가 아닌 알파벳이 있으면 브레이크
+                int wordFirstIndex = word.indexOf(currentWord);
+                int wordLastIndex = word.lastIndexOf(currentWord);
+
+                if(wordLastIndex - wordFirstIndex == 0){
+                    check = true;
+                } else if(wordLastIndex - wordFirstIndex == (currentWordCnt - 1)){
+                    check = true;
+                }
+
+                if(!check){
+                    break;
+                }
+            }
+
+            if(!check){
+                continue;
+            }
+            notDuplicateWordCnt += 1;
         }
-        System.out.println((croWordLength + croatiaWord.replaceAll(" ","").length()));
+
+        System.out.println(notDuplicateWordCnt);
+
     }
 }
