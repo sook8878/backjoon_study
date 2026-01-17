4f44faca8a5441c7587b6052799d3f1e3d0398ca
sook8878 <qpwlxk123@gmail.com>
2026-01-18 00:28:38 +0900

    2563. 색종이

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,30 +1,36 @@
 package class3;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[][] matrix = new String[5][15];
-        for (int i = 0; i < matrix.length; i++) {
-            String line = br.readLine();
-            String[] word = line.split("");
-            for (int j = 0; j < line.length(); j++) {
-                matrix[i][j] = word[j];
-            }
-        }
+        int[][] paintedArea = new int[100][100];
 
-        String result = "";
-        for (int j = 0; j < 15; j++) {
-            for (int i = 0; i < 5; i++) {
-                String word = matrix[i][j];
-                if(word != null){
-                    result += word;
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            // 2  12
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    paintedArea[j][k] = 1;
                 }
             }
         }
 
-        System.out.println(result);
+        int count = 0;
+        for(int[] a : paintedArea){
+            for(int b : a){
+                if(b == 1){
+                    count += 1;
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
