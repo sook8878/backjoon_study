94e39cef8c29ad98ec8ab5857d240285f90592fe
sook8878 <qpwlxk123@gmail.com>
2026-01-17 23:32:21 +0900

    10798. 세로읽기

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,29 +1,30 @@
 package class3;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[][] matrix = new int[9][9];
-        int maxNum = 0;
-        int row = 0;
-        int column = 0;
-        for(int i = 0; i < matrix.length; i++){
-            StringTokenizer line = new StringTokenizer(br.readLine());
+        String[][] matrix = new String[5][15];
+        for (int i = 0; i < matrix.length; i++) {
+            String line = br.readLine();
+            String[] word = line.split("");
+            for (int j = 0; j < line.length(); j++) {
+                matrix[i][j] = word[j];
+            }
+        }
 
-            for(int j = 0; j < matrix[i].length; j++){
-                int num = Integer.parseInt(line.nextToken());
-                if(maxNum <= num){
-                    maxNum = num;
-                    row = i+1;
-                    column = j+1;
+        String result = "";
+        for (int j = 0; j < 15; j++) {
+            for (int i = 0; i < 5; i++) {
+                String word = matrix[i][j];
+                if(word != null){
+                    result += word;
                 }
             }
         }
-        System.out.println(maxNum);
-        System.out.println(row + " " + column);
+
+        System.out.println(result);
     }
 }
