bd948a578174b154e229ab261c3fb0dd7d125894
sook8878 <qpwlxk123@gmail.com>
2026-01-19 00:03:23 +0900

    2745. 진법 변환

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -6,31 +6,23 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String word = st.nextToken();
+        int nBase = Integer.parseInt(st.nextToken());
 
-        int[][] paintedArea = new int[100][100];
-
-        int t = Integer.parseInt(br.readLine());
-        for (int i = 0; i < t; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            // 2  12
-            for (int j = x; j < x + 10; j++) {
-                for (int k = y; k < y + 10; k++) {
-                    paintedArea[j][k] = 1;
-                }
+        long result = 0;
+        int index = 0;
+        for (int i = word.length()-1; i >= 0; i--) {
+            int num;
+            if (word.charAt(i) >= 65){
+                num = word.charAt(i) - 55;
+            } else {
+                num = word.charAt(i) - 48;
             }
-        }
 
-        int count = 0;
-        for(int[] a : paintedArea){
-            for(int b : a){
-                if(b == 1){
-                    count += 1;
-                }
-            }
+            long baseResult = (long) Math.pow(nBase, index++);
+            result += ((long) num * baseResult);
         }
-        System.out.println(count);
+        System.out.println(result);
     }
 }
