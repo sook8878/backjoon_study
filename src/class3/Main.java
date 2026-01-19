314579fbfedde2faf29f447f9a741829d769572a
sook8878 <qpwlxk123@gmail.com>
2026-01-19 17:00:42 +0900

    2903. 중앙 이동 알고리즘

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -5,27 +5,9 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int t = Integer.parseInt(br.readLine());
-        int quarter = 25;
-        int dime = 10;
-        int nickel = 5;
-        int penny = 1;
-        for (int i = 0; i < t; i++) {
-            int restDollar = Integer.parseInt(br.readLine());
-
-            int useQuarter = restDollar / quarter;
-            restDollar = restDollar - (useQuarter * quarter);
-
-            int useDime = restDollar / dime;
-            restDollar = restDollar - (useDime * dime);
-
-            int useNickel = restDollar / nickel;
-            restDollar = restDollar - (useNickel * nickel);
-
-            int usePenny = restDollar / penny;
-
-            System.out.println(useQuarter + " " + useDime + " "  + useNickel + " "  + usePenny);
-        }
+        long pow = (long) Math.pow(2, n) + 1;
+        System.out.println(pow * pow);
     }
 }
