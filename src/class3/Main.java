cec0fd2a5eb5e2498c988fe1a7345dc5d23e7b8e
sook8878 <qpwlxk123@gmail.com>
2026-01-23 11:39:05 +0900

    2581. 소수

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,36 +1,45 @@
 package class3;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        int count = 0;
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(st.nextToken());
+        int sum = 0;
+        int minPrime = 0;
+        boolean check = false;
+        for (int i = m; i <= n; i++) {
             // 1은 소수 아님
-            if(n == 1){
+            if(i == 1){
                 continue;
             }
 
             boolean isPrime = true;
-            for (int j = 2; j < n; j++) {
-                if(n % j == 0){
+            for (int j = 2; j < i; j++) {
+                if(i % j == 0){
                     isPrime = false;
                     break;
                 }
             }
 
             if(isPrime){
-                count++;
+                sum += i;
+                if(!check){
+                    minPrime = i;
+                    check = true;
+                }
             }
         }
 
-        System.out.println(count);
+        if(sum == 0){
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(minPrime);
+        }
     }
 }
