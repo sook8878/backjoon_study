d0ee9ea6aedff917ffeb79f77fba03b17279225a
sook8878 <qpwlxk123@gmail.com>
2026-01-23 11:29:29 +0900

    1978. 소수 찾기

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,47 +1,36 @@
 package class3;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String line;
-        while ((line = br.readLine()) != null) {
-            int n = Integer.parseInt(line);
-            if (n == -1) {
-                return;
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int count = 0;
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            // 1은 소수 아님
+            if(n == 1){
+                continue;
             }
 
-            int index = 0;
-            int[] factors = new int[n];
-            for (int i = 1; i < n; i++) {
-                if (n % i == 0) {
-                    factors[index] = i;
-                    index++;
-                }
-            }
-
-            String result = n + " = ";
-            int sum = 0;
-            for (int i = 0; i < factors[i]; i++) {
-
-                int factor = factors[i];
-                if (factor == 0) {
+            boolean isPrime = true;
+            for (int j = 2; j < n; j++) {
+                if(n % j == 0){
+                    isPrime = false;
                     break;
                 }
-                sum += factor;
-                if (i == 0) {
-                    result += "" + factor;
-                } else
-                    result += " + " + factor;
             }
 
-            if (sum == n) {
-                System.out.println(result);
-            } else {
-                System.out.println(n + " is NOT perfect.");
+            if(isPrime){
+                count++;
             }
         }
+
+        System.out.println(count);
     }
 }
