3f0eb86a09a316b134c8eb0a41e6c2702460fce3
sook8878 <qpwlxk123@gmail.com>
2026-01-27 21:50:23 +0900

    10101. 삼각형 외우기

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,43 +1,24 @@
 package class3;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        if(n < 2){
-            System.out.println(0);
-            return;
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        boolean equilateral = (a + b + c == 180);
+        if(!equilateral){
+            System.out.println("Error");
+        } else if(equilateral && (a == 60 && b == 60 && c == 60)){
+            System.out.println("Equilateral");
+        } else if (equilateral && (a == b || a == c || b == c)) {
+            System.out.println("Isosceles");
+        } else {
+            System.out.println("Scalene");
         }
-
-        int minX = 0;
-        int maxX = 0;
-        int minY = 0;
-        int maxY = 0;
-        for (int i = 0; i < n; i++) {
-            StringTokenizer line = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(line.nextToken());
-            int y = Integer.parseInt(line.nextToken());
-
-            if(minX > x || minX == 0){
-                minX = x;
-            }
-            if(maxX < x || maxX == 0){
-                maxX = x;
-            }
-
-            if(minY > y || minY == 0){
-                minY = y;
-            }
-            if(maxY < y || maxY == 0){
-                maxY = y;
-            }
-        }
-
-        int extent = ((maxX - minX) * (maxY - minY));
-        System.out.println(extent);
     }
 }
