a8df297b778985f367acc8b43f1312e8f6975cca
sook8878 <qpwlxk123@gmail.com>
2026-01-27 17:16:17 +0900

    27323. 직사각형

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -6,23 +6,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
 
-        // 1은 소수 아님
-        String nStrTemp = "";
-        for (int i = 2; i <= n; i++) {
-
-            if (n % i == 0) {
-                n = (n / i);
-                nStrTemp += " " + i;
-
-                i = 1;
-            }
-        }
-
-        String[] nStrArray = nStrTemp.trim().split(" ");
-        for(String nStr : nStrArray){
-            System.out.println(nStr);
-        }
+        System.out.println(a * b);
     }
 }
