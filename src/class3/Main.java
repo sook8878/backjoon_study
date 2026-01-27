7cad5ecd8866ddf389c11c2bf94d29c7f172eaae
sook8878 <qpwlxk123@gmail.com>
2026-01-27 20:09:33 +0900

    1085. 직사각형에서 탈출

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,14 +1,29 @@
 package class3;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer line = new StringTokenizer(br.readLine());
 
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
+        int x = Integer.parseInt(line.nextToken());
+        int y = Integer.parseInt(line.nextToken());
 
-        System.out.println(a * b);
+        int w = Integer.parseInt(line.nextToken());
+        int h = Integer.parseInt(line.nextToken());
+
+        int xDistance = w - x;
+        int yDistance = h - y;
+
+        int[] arr = new int[]{xDistance, yDistance, x, y};
+        int minNum = 0;
+        for(int num : arr){
+            if(num < minNum || minNum == 0){
+                minNum = num;
+            }
+        }
+        System.out.println(minNum);
     }
 }
