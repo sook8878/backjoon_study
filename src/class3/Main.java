59b14771725ca56895d59f7c344574f49d98a177
sook8878 <qpwlxk123@gmail.com>
2026-01-27 20:54:41 +0900

    3009. 네 번째 점

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -6,24 +6,37 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer line = new StringTokenizer(br.readLine());
 
-        int x = Integer.parseInt(line.nextToken());
-        int y = Integer.parseInt(line.nextToken());
+        StringTokenizer line1 = new StringTokenizer(br.readLine());
+        StringTokenizer line2 = new StringTokenizer(br.readLine());
+        StringTokenizer line3 = new StringTokenizer(br.readLine());
+        int p1_x = Integer.parseInt(line1.nextToken());
+        int p1_y = Integer.parseInt(line1.nextToken());
 
-        int w = Integer.parseInt(line.nextToken());
-        int h = Integer.parseInt(line.nextToken());
+        int p2_x = Integer.parseInt(line2.nextToken());
+        int p2_y = Integer.parseInt(line2.nextToken());
 
-        int xDistance = w - x;
-        int yDistance = h - y;
+        int p3_x = Integer.parseInt(line3.nextToken());
+        int p3_y = Integer.parseInt(line3.nextToken());
 
-        int[] arr = new int[]{xDistance, yDistance, x, y};
-        int minNum = 0;
-        for(int num : arr){
-            if(num < minNum || minNum == 0){
-                minNum = num;
-            }
+        int x = 0;
+        if(p1_x == p2_x){
+            x = p3_x;
+        } else if (p2_x == p3_x) {
+            x = p1_x;
+        } else if (p1_x == p3_x) {
+            x = p2_x;
         }
-        System.out.println(minNum);
+
+        int y = 0;
+        if(p1_y == p2_y){
+            y = p3_y;
+        } else if (p2_y == p3_y) {
+            y = p1_y;
+        } else if (p1_y == p3_y) {
+            y = p2_y;
+        }
+
+        System.out.println(x + " " + y);
     }
 }
