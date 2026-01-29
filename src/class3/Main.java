b702615e1caa9fc4fc42d29a79a6a6113263f1af
sook8878 <qpwlxk123@gmail.com>
2026-01-29 09:59:55 +0900

    24262. 알고리즘 수업 - 알고리즘의 수행 시간 1

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,26 +1,10 @@
 package class3;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-
-        int sideSum = a+b+c;
-        if ((sideSum - a) <= a) {
-            a = b + c - 1;
-        } else if ((sideSum - b) <= b) {
-            b = a + c - 1;
-        } else if ((sideSum - c) <= c){
-            c = a + b - 1;
-        }
-
-        System.out.println(a + b + c);
+    public static void main(String[] args) {
+        System.out.println(0);
+        System.out.println(1);
     }
 }
