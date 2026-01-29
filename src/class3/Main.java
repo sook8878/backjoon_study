1dc14cd6201b7ec165fe1b41475f2f545381fc21
sook8878 <qpwlxk123@gmail.com>
2026-01-29 10:08:51 +0900

    24263. 알고리즘 수업 - 알고리즘의 수행 시간 2

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -3,8 +3,10 @@ package class3;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(0);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine());
         System.out.println(1);
     }
 }
