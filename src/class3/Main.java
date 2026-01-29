f4a1d096a5face50d66c68e83ceb165f2e85f2b2
sook8878 <qpwlxk123@gmail.com>
2026-01-29 10:22:56 +0900

    24266. 알고리즘 수업 - 알고리즘의 수행 시간 5

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -7,8 +7,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        long result = ((long) n * (n - 1)) / 2;
-        System.out.println(result);
-        System.out.println(2);
+        System.out.println((long) n * n * n);
+        System.out.println(3);
     }
 }
