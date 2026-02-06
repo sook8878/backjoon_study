6eebee0d686bc4aeac7228bd6f982d8e5509196c
sook8878 <qpwlxk123@gmail.com>
2026-02-06 18:45:05 +0900

    24313. 알고리즘 수업 - 점근적 표기 1

--- src/class3/Main.java
+++ src/class3/Main.java
@@ -1,14 +1,29 @@
 package class3;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        long result = ((long) n * ((long) (n - 1) * (n - 2))) / 6;
-        System.out.println(result);
-        System.out.println(3);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
+
+        // f(n) 은 a1n + a0
+        // (a1n + a0) <= c * n0
+        // 7n+7
+        int x = (a1 * n0) + a0;
+        int y = c * n0;
+//        System.out.println("X : "+ x + ", Y : " + y);
+        if(x <= y && a1 <= c){
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
     }
 }
